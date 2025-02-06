fdc2f550e86b68902a761cedea9faccb1016e9b1
tiagovattosz@gmail.com <tiagovattosz@gmail.com>
2025-02-06 15:02:09 -0300

    remove default tests and add money test with jasmine

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,23 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe("test suite: formatCurrency", () => {
+  it("convert cents into dollars", () => {
+    expect(formatCurrency(2095)).toEqual("20.95");
+  });
+
+  it("works with 0", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+
+  it("rounds up to nearest cent", () => {
+    expect(formatCurrency(2000.8)).toEqual("20.01");
+  });
+
+  it("rounds up to nearest cent", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+  });
+
+  it("rounds down to nearest cent", () => {
+    expect(formatCurrency(2000.1)).toEqual("20.00");
+  });
+});
