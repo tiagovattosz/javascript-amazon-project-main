b1f519c20fbdcfef9e90009af676a065e46f4e9a
Tiago Vattos Zamboni <tiagovattosz@gmail.com>
2025-02-12 10:32:03 -0300

    load products from backend

--- data/products.js
+++ data/products.js
@@ -44,7 +44,7 @@ class Clothing extends Product {
   sizeChartLink;
 
   constructor(productDetails) {
-    super(productDetails) ;
+    super(productDetails);
     this.sizeChartLink = productDetails.sizeChartLink;
   }
 
@@ -55,6 +55,26 @@ class Clothing extends Product {
   }
 }
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    fun();
+  });
+
+  xhr.open("GET", "https://supersimplebackend.dev/products");
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -532,3 +552,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,9 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import { loadProducts } from '../data/products.js';
+// import './backend-practice.js';
 // import "../data/car.js";
-
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
