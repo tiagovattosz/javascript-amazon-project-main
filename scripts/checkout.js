a798089ca7050737b0c78805b4363b38c487a4fa
Tiago Vattos Zamboni <tiagovattosz@gmail.com>
2025-02-13 09:55:00 -0300

    convert loadProducts from HXR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,16 +1,12 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 // import './backend-practice.js';
 // import "../data/car.js";
 
 Promise.all([
-  new Promise((resolve) => {
-    loadProducts(() => {
-      resolve("value 1");
-    });
-  }),
+  loadProductsFetch(),
   new Promise((resolve) => {
     loadCart(() => {
       resolve();
